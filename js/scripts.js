e6ecf75306c733878105a808d9e87e1e96afc149
Student <example@example.com>
2019-06-26 19:37:58 -0700

    updated logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,47 @@
-var inputnumber = prompt("enter a number");
+// var inputnumber = prompt("enter a number");
         
-var numberset = [];
-for (var index = 1; index <= inputnumber; index += 1) {
-    numberset.push(index);
-    };
-alert(numberset);
+// var numberset = [];
+// for (var index = 1; index <= inputnumber; index += 1) {
+//     numberset.push(index);
+//     };
+// alert(numberset);
 
-numberset.forEach(function(number) {
-    if (number % 3 && number % 5) {
-        ("ping pong").append("ul#output")
-    } else if (number % 3) {
-        ("ping").append("ul#output")
-    } else if (number % 5) {
-        ("pong").append("ul#output")
-    } else {
-        (number).append("ul#output")
-    }
-})
+// numberset.forEach(function(number) {
+//     if (number % 3 && number % 5) {
+//         ("ping pong").append("ul#output")
+//     } else if (number % 3) {
+//         ("ping").append("ul#output")
+//     } else if (number % 5) {
+//         ("pong").append("ul#output")
+//     } else {
+//         (number).append("ul#output")
+//     }
+// })
 
 
-// $(document).ready(function() {
-//     $("form#input").submit(function(event) {
-//         event.preventDefault();
-//         var inputnumber = parseInt($(#usernumber).val())
+$(document).ready(function() {
+    $("form#input").submit(function(event) {
+        event.preventDefault();
+        var inputnumber = parseInt($("input#usernumber").val())
         
 
-//         var numberset = [];
-//         for (var index = 0; index <= inputnumber; index += 1) {
-//             numberset.push(index);
-//         };
-//         alert(numberset);
-//     });
-// });
+        var numberset = [];
+        for (var index = 1; index <= inputnumber; index += 1) {
+            numberset.push(index);
+        };
+        numberset.forEach(function(number) {
+            if (number % 3 && number % 5) {
+                ("ul#output").append("<li>ping pong</li>")
+            } else if (number % 3) {
+                ("ul#output").append("<li>ping</li>")
+            } else if (number % 5) {
+                ("ul#output").append("<li>pong</li>")
+            } else {
+                ("ul#output").append("<li>" + number + "</li>")
+            }
+        })
+    });
+});
 
 // number-set.forEach(function(number) {
 
