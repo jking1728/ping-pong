5717e7b506700bacef56d8bb6a8b36c50ff7cc2d
Jordan King <supapeachx@gmail.com>
2019-06-27 21:17:26 -0700

    updated logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,3 @@
-// var inputnumber = prompt("enter a number");
-        
-// var numberset = [];
-// for (var index = 1; index <= inputnumber; index += 1) {
-//     numberset.push(index);
-//     };
-// alert(numberset);
-
-// numberset.forEach(function(number) {
-//     if (number % 3 && number % 5) {
-//         ("ping pong").append("ul#output")
-//     } else if (number % 3) {
-//         ("ping").append("ul#output")
-//     } else if (number % 5) {
-//         ("pong").append("ul#output")
-//     } else {
-//         (number).append("ul#output")
-//     }
-// })
-
-
 $(document).ready(function() {
     $("form#input").submit(function(event) {
         event.preventDefault();
@@ -44,7 +23,3 @@ $(document).ready(function() {
         
     });
 });
-
-// number-set.forEach(function(number) {
-
-// })
